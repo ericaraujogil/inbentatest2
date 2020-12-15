f5db320bfb5e3aa5b8201a128ff404e852736310
ericaraujogil <ericaraujogil@gmail.com>
2020-12-15 15:14:28 +0100

    no message

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     $headers = [
-    'x-inbenta-key' => '{{nyUl7wzXoKtgoHnd2fB0uRrAv0dDyLC+b4Y6xngpJDY=}}',
+    'x-inbenta-key' => 'nyUl7wzXoKtgoHnd2fB0uRrAv0dDyLC+b4Y6xngpJDY=',
     'Content-Type' => 'application/json'
     ];
     $body = [
-    'secret' => '{{eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0IjoieW9kYV9jaGF0Ym
+    'secret' => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0IjoieW9kYV9jaGF0Ym
         90X2VuIn0.anf_eerFhoNq6J8b36_qbD4VqngX79-yyBKWih_eA1-
-        HyaMe2skiJXkRNpyWxpjmpySYWzPGncwvlwz5ZRE7eg}}'
+        HyaMe2skiJXkRNpyWxpjmpySYWzPGncwvlwz5ZRE7eg'
     ];
     $response = $req->post('https://api.inbenta.io/v1/auth', $headers, $body);
     $response = json_decode($response);
